10f471a7c3ec47e90e3a8c849e97427dc3bcca5f
VladBogda <vladbogdantsev@gmail.com>
2023-07-11 12:08:43 +0300

    flyweight

---
+++ Pattern-structural/src/main/java/vladproduction/flyweight/InventoryExample/Item.java
@@ -0,0 +1,14 @@
+package vladproduction.flyweight.InventoryExample;
+
+//Instances of Item will be the Flyweights
+class Item {
+	private final String name;
+
+	public Item(String name) {
+		this.name = name;
+	}
+
+	public String toString() {
+		return name;
+	}
+}
